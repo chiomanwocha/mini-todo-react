c3f965b37db7cb401f65bf396ec18b96f8d9fa72
Chioma Nwocha <chiomanwocha@Chiomas-MacBook-Pro.local>
2022-12-20 12:37:03 +0100

    modified the signup page

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import LoginTodo from './todo-components/LoginTodo';
 import SignUp from './todo-components/SignUp';
 import TodoApp from './todo-components/TodoApp';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <LoginTodo></LoginTodo>
-          </Route>
-          <Route path="/signup">
-            <SignUp></SignUp>
-          </Route> 
-          <Route path="/:id">
-            <TodoApp></TodoApp>
-          </Route> 
-        </Switch>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <LoginTodo></LoginTodo>
+            </Route>
+            <Route path="/signup">
+              <SignUp></SignUp>
+            </Route> 
+            <Route path="/:id">
+              <TodoApp></TodoApp>
+            </Route> 
+          </Switch>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/todo-components/SignUp.js
+++ src/todo-components/SignUp.js
@@ -3,75 +3,69 @@ import { useState } from 'react'
 import '../css/signup.css'
 import axios from 'axios'
 import Loader from './Loader'
+import { useMutation } from 'react-query'
 
 const SignUp = () => {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
-    const [userExist, setUserExist] = useState(false)
     const [redirect, setRedirect] = useState(false)
-    const [loader, setLoader] = useState(false)
+    
+    const registerUser = (details) => {
+        return axios.post('https://todo-api-12iv.onrender.com/users', details)
+    }
 
-    const createAccount = (e) => {
-        setLoader(true)
-        e.preventDefault()
-        setUserExist(true)
-        axios
-        .get('https://6391a596b750c8d178c8e2e7.mockapi.io/users')
-        .then((response) => {
-            ((response.data).find((user) => user.email === email) === undefined) ?
-            axios
-            .post('https://6391a596b750c8d178c8e2e7.mockapi.io/users', {
-                firstName: firstName,
-                lastName: lastName,
-                email: email
-            })
-            .then(() =>{
-                alert('Registered successfully')
-                setRedirect(true)
-            }
-            )
-            .catch((error) => {
-                alert(error);
-            })
-            : setLoader(false) && setUserExist(true)  
-        })
-        .catch((error) => {
-            alert(error)
-        })
+    const { mutate: register, status, error} = useMutation(registerUser,
+    {
+        onSuccess: (data) => {
+            alert(data.data.message)
+            setRedirect(true)
+        }
+    })
 
+    const createAccount = (e) =>{
+        e.preventDefault();
+        const details = {
+            fisrtname : firstName,
+            lastname: lastName,
+            email: email
+        }
+        register(details)
         setFirstName('')
         setLastName('')
         setEmail('')
     }
+
     if(redirect){
-        return <Redirect to='/' />
+       return <Redirect to='/' />
     }
+
     return (
         <div className="container">
-            <form className="signup-page"  onSubmit={(e) => createAccount(e)}>
+            <form className="signup-page" onSubmit={(e) => createAccount(e)}>
                 <div className="welcome-hero">
                     <p>Create Account</p>
                     <p>to get started now !</p>
                 </div>
-                {loader ? 
-                <div className='signup-loader'>
-                    <Loader></Loader> 
-                </div>
-                :
-                <div>
-                    {userExist && <p className='error'>Please register with another email, email already exist !</p>}
-                    <div className="firstname">
-                        <input type="text" id="firstname" name="firstname" placeholder="First Name" required className="signup-details" value={firstName} onChange={(e) => setFirstName(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))} autoFocus/>
+                {status === 'loading' &&
+                    <div className='signup-loader'>
+                        <Loader></Loader> 
                     </div>
-                    <div className="lastname">
-                        <input type="text" id="lastname" name="lastname" placeholder="Last Name" required className="signup-details" value={lastName} onChange={(e) => setLastName(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))}/>
-                    </div>
+                }
+                {status !== 'loading' &&
                     <div>
-                        <input type="email" id="email" name="email" placeholder="Email Address" required className="signup-details" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        {status === 'error' && <p className='error'>{error.message}</p>}
+                        <div className="firstname">
+                            <input type="text" id="firstname" name="firstname" placeholder="First Name" required className="signup-details" value={firstName} onChange={(e) => setFirstName(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))} autoFocus/>
+                        </div>
+                        <div className="lastname">
+                            <input type="text" id="lastname" name="lastname" placeholder="Last Name" required className="signup-details" value={lastName} onChange={(e) => setLastName(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1))}/>
+                        </div>
+                        <div>
+                            <input type="email" id="email" name="email" placeholder="Email Address" required className="signup-details" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                        <button className="signup-button">Submit</button>
                     </div>
-                    <button className="signup-button">Submit</button>
-                </div>
                 }
                 <p>Already have an account ? <Link to="/">login</Link></p>
             </form>
